13315e3b430733d8b164fcc02e40b4ce9f1e0fc1
Ethankimmy <ethan.kimmy@gmail.com>
2023-10-11 22:44:02 -0500

    fixed code

--- practice.js
+++ practice.js
@@ -70,7 +70,7 @@ function pickComputerMove() {
   const randomNumber = Math.random();
 
   let computerMove = '';
- k
+ 
   if (randomNumber >= 0 && randomNumber < 1 / 3) {
     computerMove = 'rock';
   } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
